798b0842124dcf83e85ae0bd77b8ee575df9f07a
ardaatahan <63500745+ardaatahan@users.noreply.github.com>
2022-05-15 14:27:39 +0300

    add admin, student, teacher models

--- levelup_api/models.py
+++ levelup_api/models.py
@@ -5,3 +5,26 @@ from django.contrib.auth.models import User
 class User(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20)
+
+
+class SystemUser(models.Model):
+    user = models.OneToOneField("levelup_api.User", on_delete=models.CASCADE)
+    dateBirth = models.DateField()
+    photo = models.ImageField()
+
+
+class SystemAdmin(models.Model):
+    user = models.OneToOneField("levelup_api.User", on_delete=models.CASCADE)
+
+
+class Student(models.Model):
+    user = models.OneToOneField("levelup_api.User", on_delete=models.CASCADE)
+    contactNo = models.CharField(max_length=12)
+    # add level
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField("levelup_api.User", on_delete=models.CASCADE)
+    description = models.TextField()
+    rating = models.DecimalField()
+    yearsOfExperience = models.IntegerField()
