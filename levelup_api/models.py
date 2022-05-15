b822f0947867ebcfae6d00087fbb0355c5ee3ac6
ardaatahan <63500745+ardaatahan@users.noreply.github.com>
2022-05-15 14:44:01 +0300

    implement models until hw upload

--- levelup_api/models.py
+++ levelup_api/models.py
@@ -23,7 +23,7 @@ class Student(models.Model):
     user = models.OneToOneField(
         "levelup_api.User", on_delete=models.CASCADE, primary_key=True)
     contactNo = models.CharField(max_length=12)
-    # add level
+    # todo: add level
 
 
 class Teacher(models.Model):
@@ -42,4 +42,37 @@ class LanguageNative(models.Model):
 
 
 class Class(models.Model):
-    pass
+    capacity = models.IntegerField()
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.IntegerField()
+    startDate = models.DateField()
+    endDate = models.DateField()
+    weeklyPlan = models.TextField()
+    enrollment = models.IntegerField()
+    # todo: lang_id INT NOT NULL,
+    teacher = models.OneToOneField("levelup_api.Teacher")
+    # todo: level_id INT NOT NULL,
+    image = models.ImageField()
+
+
+class SpeakingExercise(models.Model):
+    exerciseLink = models.TextField()
+    exerciseDatetime = models.DateTimeField()
+    grade = models.FloatField()
+    student = models.OneToOneField("levelup.Student")
+    # todo: lang_id INT NOT NULL,
+    languageNative = models.OneToOneField("levelup.LanguageNative")
+
+
+class ClassBook(models.Model):
+    bookName = models.TextField()
+
+
+class Homework(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    dueDatetime = models.DateTimeField()
+    assignDatetime = models.DateTimeField()
+    grade = models.FloatField()
+    givenClass = models.OneToOneField("levelup.Class")
