c20242d622a1034ee6bdfe0d7d27b662cf834d4c
ardaatahan <63500745+ardaatahan@users.noreply.github.com>
2022-05-15 13:14:44 +0300

    setup postgres

--- levelup/settings.py
+++ levelup/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'levelup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'levelup_db',
+        'USER': 'postgres',
+        'PASSWORD': 'arda2001',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
